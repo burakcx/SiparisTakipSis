4690a5ed28ed001c958b10f82af309189524b1ac
Burak Coşkun <burakc34@gmail.com>
2018-07-14 12:45:18 +0300

    Yeni versiyonlar eklendi
    Yeni versiyonlar eklendi

---
+++ SiparisTakip.AspNetMvcWebApi/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SiparisTakip.AspNetMvcWebApi.Startup))]
+
+namespace SiparisTakip.AspNetMvcWebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ SiparisTakip.Dal/Concrete/EntityFramework/Repository/EFProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using SiparisTakip.Dal.Abstract.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SiparisTakip.Entity.Models;
+
+namespace SiparisTakip.Dal.Concrete.EntityFramework.Repository
+{
+    public class EFProductRepository : IProductDal
+    {
+        public Products Getir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Guncelle(Products nesne)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Products Kaydet(Products nesne)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Products> Listele()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Sil(Products nesne)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Sil(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
